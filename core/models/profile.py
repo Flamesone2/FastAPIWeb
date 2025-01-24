cfffd68a82b9a0df0b63c174c5f638d11fc2073d
Flamesone2 <sergbasov.com@gmail.com>
2025-01-24 13:04:48 +0300

    Formatted class Profile with black, fixed annotation on first and last names

--- core/models/profile.py
+++ core/models/profile.py
@@ -2,16 +2,18 @@ from .base import Base
 from sqlalchemy.orm import (
     Mapped,
     mapped_column,
-    relationship,
 )
-from sqlalchemy import String, ForeignKey
-from typing import TYPE_CHECKING
+from sqlalchemy import String
 from .mixins import UserRelationMixin
 
 
 class Profile(UserRelationMixin, Base):
     _user_id_unique = True
     _user_back_populates = "profile"
-    first_name: Mapped[str] = mapped_column(String(40))
-    last_name: Mapped[str] = mapped_column(String(40))
+    first_name: Mapped[str | None] = mapped_column(
+        String(40)
+    )
+    last_name: Mapped[str | None] = mapped_column(
+        String(40)
+    )
     bio: Mapped[str | None]
