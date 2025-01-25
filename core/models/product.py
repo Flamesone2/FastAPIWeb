49ba23ac2e30777ba1df4c5e2996260233ef34c8
Flamesone2 <sergbasov.com@gmail.com>
2025-01-25 14:58:30 +0300

    Added changes (orders_details column) to "product" to sync with "order" table by "order_product_association" table

--- core/models/product.py
+++ core/models/product.py
@@ -1,5 +1,14 @@
 from core.models.base import Base
-from sqlalchemy.orm import Mapped
+from sqlalchemy.orm import Mapped, relationship
+
+from typing import TYPE_CHECKING
+
+
+if TYPE_CHECKING:
+    from .order import Order
+    from .order_product_association import (
+        OrderProductAssociation,
+    )
 
 
 class Product(Base):
@@ -7,3 +16,11 @@ class Product(Base):
     name: Mapped[str]
     description: Mapped[str]
     price: Mapped[int]
+
+    # orders: Mapped[list["Order"]] = relationship(
+    #     secondary="order_product_association",
+    #     back_populates="products",
+    # )
+    orders_details: Mapped[list["OrderProductAssociation"]] = relationship(
+        back_populates="product",
+    )
